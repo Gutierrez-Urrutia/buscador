27e73f1e995744edbf7136e318ac1d21aa22915d
Pablo Gutiérrez <pa.gutierrezu@duocuc.cl>
2025-03-13 11:28:57 -0300

    Buscador con Angular y Spring

---
+++ src/app/search/search.component.ts
@@ -0,0 +1,24 @@
+import {Component, OnInit} from '@angular/core';
+import { Person, SearchService} from "../shared";
+
+@Component({
+  selector: 'app-search',
+  templateUrl: './search.component.html',
+  styleUrls: ['./search.component.css']
+})
+export class SearchComponent implements OnInit {
+
+  query!: string;
+  searchResults: Person[] = [];
+  constructor(private searchService: SearchService){};
+  ngOnInit(): void {};
+  search(): void {
+    this.searchService.search(this.query).subscribe({
+      next: (data: Person[]) => {
+        this.searchResults = data;
+      },
+      error: (error: any) => console.log(error)
+    });
+  }
+
+}
---
+++ src/app/shared/search/search.service.ts
@@ -0,0 +1,59 @@
+
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {map, Observable} from "rxjs";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SearchService {
+
+  constructor(private http: HttpClient ) { }
+  getAll(): Observable<Person[]> {
+    return this.http.get<Person[]>('assets/data/people.json');
+  }
+
+  search(q: string): Observable<Person[]>{
+    if(!q || q === '*'){
+      q = '';
+    } else {
+      q = q.toLowerCase();
+    }
+    return this.getAll().pipe(
+      map((data: Person[]) => {
+        return data.filter((item: Person) =>
+          JSON.stringify(item).toLowerCase().includes(q)
+        );
+      })
+    )
+  };
+}
+
+export class Address {
+  street: string;
+  city: string;
+  state: string;
+  zip: string;
+
+  constructor(address: Partial<Address> ={}){
+  this.street = address?.street || '';
+  this.city = address?.city || '';
+  this.state = address?.state || '';
+  this.zip = address?.zip || '';
+
+  }
+}
+
+export class Person{
+  id: number | null;
+  name: string;
+  phone: string;
+  address: Address;
+
+  constructor(person: Partial<Person>){
+    this.id = person?.id || null;
+    this.name = person?.name || "";
+    this.phone = person?.phone || "";
+    this.address = person?.address || new Address();
+  }
+}
